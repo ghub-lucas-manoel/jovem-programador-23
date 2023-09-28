62246504206611efce26bed1825ee8ce8cf029eb
Matheusiak <iakuuschv@outlook.com>
2023-09-28 01:37:34 -0300

    Arquivos Alterados - Tarefa Aula-02

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-const pesquisado = document.getElementById("pesquisado");
+const imagemMenu = document.getElementById("imgMenu");
+const Menu = document.getElementById("menuHamb");
+const menuLista = document.getElementById("list");
+const menuAberto = document.getElementById("menAberto");
+
+let menuVisivel = false;
+
+imagemMenu.addEventListener("click", () => {
+    menuVisivel = !menuVisivel;
+
+    if (menuVisivel) {
+        menuLista.classList.add("visible");
+        menuAberto.src = "_imagens/menuAberto.png";
+    } else {
+        menuLista.classList.remove("visible");
+        menuAberto.src = "_imagens/retangulos-de-menu.png";
+    }
+});
+
+const caixaTexto = document.getElementById("procurar");
 const pesquisaFeita = document.getElementById("pesquisaFeita");
 
 function mostrarPesquisa() {
-    pesquisaFeita.innerHTML = pesquisado.value;
+    pesquisaFeita.innerHTML = caixaTexto.value;
+}
+
+// Mostrar mensagem indicando em qual página o usuário está.
+function mostrarMensagem(texto) {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.textContent = `Você está em ${texto}`;
+    mensagem.style.display = "block";
 }
