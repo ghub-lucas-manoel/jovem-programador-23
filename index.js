034873d50fea9b11fe142d462b2036a07977737b
Tiago Oliveira <tiago.moliveira71@gmail.com>
2023-09-29 21:38:35 -0300

    Pagina para casarao eventos com paginas

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-function exibeNome (){
-document.getElementById("nomeDigitado").innerHTML = document.getElementById("inputNome").value;
-}
+
+    
+        
+        function navegar(pagina) {
+            
+            const botoesMenu = document.querySelectorAll("#menu button");
+            botoesMenu.forEach(botao => botao.classList.remove("active"));
+
+            
+            const paginas = document.querySelectorAll("#conteudo > div");
+            paginas.forEach(paginaElement => {
+                paginaElement.style.display = "none";
+            });
+
+            
+            document.querySelector(`#menu button[data-pagina="${pagina}"]`).classList.add("active");
+
+            
+            document.getElementById(`pagina-${pagina}`).style.display = "block";
+        }
+
+        
+        navegar("pagina-inicio");
+  
