668353f7c59aafe6d12b6a4bacacf3bce53c5fce
Guint3r <guiiiinter@gmail.com>
2023-10-01 23:05:32 -0300

    Arquivo 02

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
-function exibeNome() {
-    document.getElementById("nomeDigitado").innerHTML = document.getElementById("inputNome").value;
+const imagemMenu = document.getElementById("imgMenu");
+const Menu = document.getElementById("menu");
+const menuLista = document.getElementById("list");
+const menus = document.getElementById("menAberto");
+
+let menuVisivel = false;
+
+const caixaTexto = document.getElementById("procurar");
+const pesquisaFeita = document.getElementById("pesquisaFeita");
+
+function mostrarPesquisa() {
+    pesquisaFeita.innerHTML = caixaTexto.value;
+}
+
+function mostrarMensagem(texto) {
+    mensagem.textContent = `Você está na página ${texto}.`;
+    mensagem.className = `${classeCor}`;
+    mensagem.style.display = "block";
 }
