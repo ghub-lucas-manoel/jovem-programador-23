88e27a9c0324442bc18c39da62da4ec6c9745cd0
Matheusiak <iakuuschv@outlook.com>
2023-10-03 17:35:43 -0300

    Integração com Bootstrap

--- index.js
+++ index.js
@@ -1,22 +1,3 @@
-const imagemMenu = document.getElementById("imgMenu");
-const Menu = document.getElementById("menuHamb");
-const menuLista = document.getElementById("list");
-const menuAberto = document.getElementById("menAberto");
-
-let menuVisivel = false;
-
-imagemMenu.addEventListener("click", () => {
-    menuVisivel = !menuVisivel;
-
-    if (menuVisivel) {
-        menuLista.classList.add("visible");
-        menuAberto.src = "_imagens/menuAberto.png";
-    } else {
-        menuLista.classList.remove("visible");
-        menuAberto.src = "_imagens/retangulos-de-menu.png";
-    }
-});
-
 const caixaTexto = document.getElementById("procurar");
 const pesquisaFeita = document.getElementById("pesquisaFeita");
 
@@ -24,9 +5,8 @@ function mostrarPesquisa() {
     pesquisaFeita.innerHTML = caixaTexto.value;
 }
 
-// Mostrar mensagem indicando em qual página o usuário está.
-function mostrarMensagem(texto) {
+function mostrarMensagem(nomeDaPagina) {
     const mensagem = document.getElementById("mensagem");
-    mensagem.textContent = `Você está em ${texto}`;
+    mensagem.textContent = `Você está em ${nomeDaPagina}`;
     mensagem.style.display = "block";
 }
