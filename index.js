651b11aa98c45c9091ac4ecbc108a0111b19ab87
Lucas Manoel <lucas.manoel@youdeserve.today>
2023-10-16 22:13:40 -0300

    Aula 07 - JS: Bibliotecas e APIs

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-function exibirNome() {    
-    document.getElementById("nomeDigitado").innerHTML = document.getElementById("inputNome").value;
-}
+
+$("#botaoEnviarDados").on("click", () => {
+
+
+    var dadosPessoas = {
+        nome: "Lucas Manoel",
+        idade: 20,
+        cidade: "Chapeté",
+        dinheiroNoBanco: 1000.05,
+        temCarro: true,
+        tipoSanguineo: "AB+"
+    }
+
+    return console.log(dadosPessoas);
+
+    const serviceID = "service_i77z57q";
+    const templateID = "template_2fvt7v5";
+    // const templateParams = 
+
+    return emailjs.send(serviceID, templateID, templateParams);
+    
+});
