0c6b75f250d2b920f32436c43e7fdf509c23d1a2
Dust C <137210879+Micael-Dombroski@users.noreply.github.com>
2023-10-17 22:52:58 -0300

    Comentarios Enviaveis

--- index.js
+++ index.js
@@ -10,3 +10,19 @@ function mostrarMenu() {
         menuOculto.style.display = 'block';
     }
 }
+
+function comentar() {
+    const serviceID = "service_4vfyaeg";
+    const templateID = "template_touhztt";
+    const templateParams = {
+        comentario: $("#inputComentario").val()
+    }
+    return emailjs.send(serviceID, templateID, templateParams)
+        .then((obj, params) => {
+            $("#inputComentario").val("");
+            alert("Seu comentário foi enviado com sucesso. A equipe responsável embreve irá liberá-lo para ser exibido na página.")
+        })
+        .catch((err) => {
+            alert("Houve um erro inesperado e seu comentário não pode ser enviados. Por favor, tente novamente mais tarde.")
+        });
+};
