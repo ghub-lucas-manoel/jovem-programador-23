a09720adcc77b2483b845c30bf718d46270d5f0b
Matheusiak <iakuuschv@outlook.com>
2023-10-23 18:28:26 -0300

    Exercício de Casa - Integração EmailJS

--- index.js
+++ index.js
@@ -10,3 +10,29 @@ function mostrarMensagem(nomeDaPagina) {
     mensagem.textContent = `Você está em ${nomeDaPagina}`;
     mensagem.style.display = "block";
 }
+
+$("#botaoEnviarFeedback").on("click", () => {
+    const serviceID = "service_fxvb77a";
+    const templateID = "template_ebgb6rh";
+    const templateParams = {
+        nome: $("#inputNome").val(),
+        telefone: $("#inputTelefone").val(),
+        feedback: $("#inputFeedback").val(),
+    };
+
+    return emailjs
+        .send(serviceID, templateID, templateParams)
+        .then((obj, params) => {
+            nome: $("#inputNome").val("");
+            telefone: $("#inputTelefone").val("");
+            Feedback: $("#inputFeedback").val("");
+            alert(
+                "Seu Feedback foi enviado com sucesso, agradecemos e boas compras!"
+            );
+        })
+        .catch((err) => {
+            alert(
+                "Houve um erro inesperado, por favor tente novamente! Se o erro persistir, espere alguns minutos para tentar novamente."
+            );
+        });
+});
