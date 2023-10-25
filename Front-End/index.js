fab9a6b38f4b7a23abcc3b43cd2ade254ff86411
Cristofer Eduardo Medeiros <cristofermedeiros85@gmail.com>
2023-10-24 23:52:05 -0300

    Adicionado Comentarios, Consertado Modal.

--- Front-End/index.js
+++ Front-End/index.js
@@ -1,4 +1,8 @@
 $("#botaoEnviarFeedback").on("click", () => {
+
+    if(!inputsSaoValidados(event)) return;
+
+    $('#botaoEnviarFeedback').attr("disabled", true)
     const serviceID = "service_um6w40g";
     const templateID = "template_g5lnlpo";
     const templateParams = {
@@ -6,28 +10,40 @@ $("#botaoEnviarFeedback").on("click", () => {
         nomeCompleto: $("#inputNome").val(),
         email: $("#inputEmail").val(),
         telefone: $("#inputTelefone").val()
-    };
+    }
 
-    emailjs.send(serviceID, templateID, templateParams)
-        .then(() => {
-            $("#inputFeedback").val("");
+    return emailjs.send(serviceID, templateID, templateParams)
+        .then((obj, params) => {
             $("#inputNome").val("");
             $("#inputEmail").val("");
             $("#inputTelefone").val("");
 
-            $("#modalFeedback").children('.modal-header').text("Confirmação de Envio de Dados");
-            $("#modalFeedback").children('.modal-body').text("Seus dados foram enviados com sucesso. A equipe responsável fará o contato assim que possível.");
-            $("#modalFeedback").children('.modal-footer').find('button').removeClass("btn-danger").addClass("btn-success");
+            $("#modalHeader").children('h5').text("Confirmação de Envio de Dados")
+            $("#modalBody").children('p').text("Seus dados foram enviados com sucesso. A equipe responsável fará o contato assim que possível.")
+            $("#modalFooter").children('button').removeClass("btn-danger").addClass("btn-success");
             return;
         })
         .catch((err) => {
-            $("#modalFeedback").children('.modal-header').text("Erro no Envio de Dados");
-            $("#modalFeedback").children('.modal-body').text("Houve um erro inesperado. Por favor, tente novamente mais tarde!");
-            $("#modalFeedback").children('.modal-footer').find('button').removeClass("btn-success").addClass("btn-danger");
+            $("#modalHeader").children('h5').text("Erro no Envio de Dados")
+            $("#modalBody").children('p').text("Houve um erro inesperado. Por favor, tente novamente mais tarde!")
+            $("#modalFooter").children('button').removeClass("btn-success").addClass("btn-danger");
             return;
         })
         .finally(() => {
-            $("#modalFeedback").modal();
-            return;
+            $('#botaoEnviarFeedback').attr("disabled", false)
+            return $("#modalFeedback").modal();
+
         });
+
 });
+
+function inputsSaoValidados(event) {
+    if (!$('.needs-validation')[0].checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.needs-validation').addClass('was-validated');
+        return false;
+    }
+    $('.needs-validation').removeClass('was-validated');
+    return true;
+}
