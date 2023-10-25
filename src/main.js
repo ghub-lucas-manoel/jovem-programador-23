b3b3c3cb87089f824a8412024ec32964954a97a3
Natan <nataniel08@protonmail.com>
2023-10-24 22:19:36 -0300

    add required to form fields

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 function sendFeedback(event) {
     event.preventDefault();
 
+    if (!inputsAreValidity(event)) return;
+    $('#send-feedback-btn').attr('disabled', true);
+
     const serviceId = 'service_bxb9fwm';
     const templateId = 'template_mmxc57h';
     const templateParams = {
@@ -9,8 +12,9 @@ function sendFeedback(event) {
     };
 
     return emailjs.send(serviceId, templateId, templateParams)
-        .then((obj, params) => {
+        .then(() => {
             $('#feedback-author').val('');
+            $('#feedback-email').val('');
             $('#feedback-content').val('');
 
             $('#modal-header').children('h5').text('Sucesso');
@@ -18,15 +22,28 @@ function sendFeedback(event) {
             $('#modal-footer').children('button').removeClass('btn-danger').addClass('btn-success');
             return;
         })
-        .catch((err) => {
+        .catch(() => {
             $('#modal-header').children('h5').text('Falha');
             $('#modal-body').children('p').text('deu Ruim');
             $('#modal-footer').children('button').removeClass('btn-sucess').addClass('btn-danger');
             return;
         })
         .finally(() => {
-            return $('#feedbackModal').modal();
+            $('#feedbackModal').modal();
+            return $('#send-feedback-btn').attr('disabled', false);
         });
+}
+
+function inputsAreValidity(event) {
+    if (!$('.needs-validation')[0].checkValidity()) {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.needs-validation').addClass('was-validated');
+        return false;
+    }
+
+    $('.needs-validation').removeClass('was-validated');
+    return true;
 }
 
 $('#send-feedback-btn').on('click', sendFeedback);
