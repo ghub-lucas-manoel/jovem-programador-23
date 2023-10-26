3520d8c38c406cd9662c301c9ee167e20ee7a2e5
Lucas Manoel <lucas.manoel@youdeserve.today>
2023-10-26 20:10:51 -0300

    Aula 10: Validações

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
+$("#botaoEnviarDados").on("click", (event) => {
 
-$("#botaoEnviarDados").on("click", () => {
+    if (!inputsSaoValidos(event)) return;
+
+    $('#botaoEnviarDados').attr("disabled", true);
+
     const serviceID = "service_i77z57q";
     const templateID = "template_2fvt7v5";
     const templateParams = {
@@ -13,9 +17,31 @@ $("#botaoEnviarDados").on("click", () => {
             $("#inputNome").val("");
             $("#inputEmail").val("");
             $("#inputTelefone").val("");
-            alert("Seus dados foram enviados com sucesso. A equipe responsável fará o contato assim que possível.");
+
+            $("#modalHeader").children("h5").text("Confirmação de Envio de Dados");
+            $("#modalBody").children("p").text("Seus dados foram enviados com sucesso. A equipe responsável fará o contato assim que possível.");
+            $("#modalFooter").children('button').removeClass("btn-danger").addClass("btn-success");
+            return;
         })
         .catch((err) => {
-            alert("Houve um erro inesperado e seus dados não puderam ser enviados. Por favor, tente novamente mais tarde.");
-        });    
+            $("#modalHeader").children("h5").text("Erro no Envio de Dados");
+            $("#modalBody").children("p").text("Houve um erro inesperado e seus dados não puderam ser enviados. Por favor, tente novamente mais tarde.");
+            $("#modalFooter").children('button').removeClass("btn-success").addClass("btn-danger");
+            return;
+        })
+        .finally(() => {
+            $("#modalInscricao").modal();
+            return $('#botaoEnviarDados').attr("disabled", false);
+        });
 });
+
+function inputsSaoValidos(event) {
+    if (!$('.needs-validation')[0].checkValidity()){
+        event.preventDefault();
+        event.stopPropagation();
+        $('.needs-validation').addClass('was-validated');
+        return false;
+    } 
+    $('.needs-validation').removeClass('was-validated');
+    return true;
+}
