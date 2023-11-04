7f76c0ee861e631d2a0d7d1d44fb2921c006d810
V4r3us1n <98497693+V4r3us1n@users.noreply.github.com>
2023-11-04 14:58:38 -0300

    Deixando o site mais acessível

--- script.js
+++ script.js
@@ -15,7 +15,11 @@ function exibirMenuLogin()
     }
 }
 
-$("#botaoEnviarDados").on("click", () => {//() => {} função anônima
+$("#botaoEnviarDados").on("click", (event) => {//() => {} função anônima
+    //TODO: Checar se campos estão validos
+    if(!inputSaoValidos(event)) return;
+
+    $("#botaoEnviarDados").attr("disabled", true);
 
     const serviceID = "service_sebuy0b";
     const templateID = "template_h122i8n";
@@ -29,9 +33,35 @@ $("#botaoEnviarDados").on("click", () => {//() => {} função anônima
             $("#nome-usuario").val("");
             $("#email-usuario").val("");
             $("#telefone-usuario").val("");
-            alert("Seu comentário foi enviado com sucesso. A equipe responsável embreve irá liberá-lo para ser exibido na página.")
+            $("#modalHeader").children('h5').text("Confirmação de Envio do Report de Erro");
+            $("#modalBody").children('p').text("Seu report foi enviado com sucesso. A equipe responsável embreve entrarará em contato.");
+            $("#modalFooter").children('button').removeClass("btn-danger").addClass("btn-success");
+
+            //alert("Seu comentário foi enviado com sucesso. A equipe responsável embreve irá liberá-lo para ser exibido na página.")
+            return;
         })
         .catch((err) => {
-            alert("Houve um erro inesperado e seu comentário não pode ser enviados. Por favor, tente novamente mais tarde.")
-        });
+            $("#modalHeader").children('h5').text("Erro no Envio de Dados");
+            $("#modalBody").children('p').text("Houve um erro inesperado e seus dados não puderam ser enviados. Por favor, tente novamente mais tarde.");
+            $("#modalFooter").children('button').removeClass("btn-success").addClass("btn-danger");
+
+            //alert("Houve um erro inesperado e seu comentário não pode ser enviados. Por favor, tente novamente mais tarde.")
+            return;
+        })
+        .finally(() => {//independente do sucesso ou falha, esse passo será executado
+            $("#modalReportErro").modal();
+            return $("#botaoEnviarDados").attr("disabled", false);
+        })
 });
+
+function inputSaoValidos(event) {
+    if (!$('.needs-validation')[0].checkValidity())
+    {
+        event.preventDefault();
+        event.stopPropagation();
+        $('.needs-validation').addClass('was-validated');
+        return false;
+    }
+    $('.needs-validation').removeClass('was-validated');
+    return true;
+}
